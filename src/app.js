3a0432b142804bd2c65156befab145ff0be28386
Kremen_Dmitriy <kremen.dima25@gmail.com>
2024-04-20 21:01:02 +0200

    video module, new folder assets

--- src/app.js
+++ src/app.js
@@ -3,40 +3,45 @@ import { ContextMenu } from './menu';
 import { BackgroundModule } from './modules/background.module';
 import { ClicksModule } from './modules/clicks.module';
 import { ShapeModule } from './modules/shape.module';
+import { VideoModule } from './modules/video.module';
+
 class App {
-	#contextMenu;
-	#backgroundModule;
-	#clicksModule;
-	#shapeModule;
+   #contextMenu;
+   #backgroundModule;
+   #clicksModule;
+   #shapeModule;
+   #videoModule;
 
-	constructor() {
-		this.#shapeModule = new ShapeModule(
-			'ShapeModule',
-			'Create figure',
-			document.body
-		);
-		this.#backgroundModule = new BackgroundModule(
-			'background',
-			'Поменять цвет фона'
-		);
-		this.#clicksModule = new ClicksModule(
-			'ClicksModule',
-			'Click',
-			document.body,
-			document.body,
-			5
-		);
-		this.#contextMenu = new ContextMenu('.menu', [
-			this.#backgroundModule,
-			this.#clicksModule,
-			this.#shapeModule,
-		]);
-	}
+   constructor() {
+      this.#shapeModule = new ShapeModule(
+         'ShapeModule',
+         'Create figure',
+         document.body
+      );
+      this.#backgroundModule = new BackgroundModule(
+         'background',
+         'Поменять цвет фона'
+      );
+      this.#clicksModule = new ClicksModule(
+         'ClicksModule',
+         'Click',
+         document.body,
+         document.body,
+         5
+      );
+      this.#videoModule = new VideoModule('video', 'Открыть Pedro-енота');
+      this.#contextMenu = new ContextMenu('.menu', [
+         this.#backgroundModule,
+         this.#clicksModule,
+         this.#shapeModule,
+         this.#videoModule,
+      ]);
+   }
 
-	run() {
-		this.#contextMenu.open();
-		this.#contextMenu.add();
-	}
+   run() {
+      this.#contextMenu.open();
+      this.#contextMenu.add();
+   }
 }
 
 const app = new App();
