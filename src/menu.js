84474eeb1e4bd71a8046f4590fca0edab80ce056
Teves <generalpokrichkin@icloud.com>
2024-04-20 19:49:36 +0300

    add css for context menu

--- src/menu.js
+++ src/menu.js
@@ -12,13 +12,6 @@ export class ContextMenu extends Menu {
 	open() {
 		document.body.addEventListener('contextmenu', event => {
 			event.preventDefault();
-			console.log(this.#itemArr);
-			// if (!this.#itemArr > 0) {
-			// 	this.el.style.top = `${event.pageY}px`;
-			// 	console.log(event.pageY);
-			// 	this.el.style.left = `${event.pageX}px`;
-			// 	console.log(this.el.classList.add('open'));
-			// }
 			this.el.style.top = `${event.pageY}px`;
 			this.el.style.left = `${event.pageX}px`;
 			this.el.classList.add('open');
@@ -30,6 +23,8 @@ export class ContextMenu extends Menu {
 		this.el.classList.remove('open');
 	}
 	add() {
+		const li = document.querySelector('li');
+		console.log(li);
 		this.#itemArr.forEach(item => {
 			if (item instanceof Module) {
 				this.el.insertAdjacentHTML('beforeend', item.toHTML());
