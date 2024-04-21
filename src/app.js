4211d402f0cea47e290c13fe65602376292c3352
Teves <generalpokrichkin@icloud.com>
2024-04-21 15:14:54 +0300

    add style for context menu

--- src/app.js
+++ src/app.js
@@ -7,45 +7,45 @@ import { VideoModule } from './modules/video.module';
 import { Message } from './modules/message.module';
 
 class App {
-   #contextMenu;
-   #backgroundModule;
-   #clicksModule;
-   #shapeModule;
-   #messageModule;
-   #videoModule;
+	#contextMenu;
+	#backgroundModule;
+	#clicksModule;
+	#shapeModule;
+	#messageModule;
+	#videoModule;
 
-   constructor() {
-	  this.#messageModule = new Message('Message', 'Рандомное сообщение') ;
-      this.#shapeModule = new ShapeModule(
-         'ShapeModule',
-         'Create figure',
-         document.body
-      );
-      this.#backgroundModule = new BackgroundModule(
-         'background',
-         'Поменять цвет фона'
-      );
-      this.#clicksModule = new ClicksModule(
-         'ClicksModule',
-         'Click',
-         document.body,
-         document.body,
-         5
-      );
-      this.#videoModule = new VideoModule('video', 'Открыть Pedro-енота');
-      this.#contextMenu = new ContextMenu('.menu', [
-         this.#backgroundModule,
-         this.#clicksModule,
-         this.#shapeModule,
-         this.#messageModule,
-         this.#videoModule,
-      ]);
-   }
+	constructor() {
+		this.#messageModule = new Message('Message', 'Рандомное сообщение');
+		this.#shapeModule = new ShapeModule(
+			'ShapeModule',
+			'Создание фигур',
+			document.body
+		);
+		this.#backgroundModule = new BackgroundModule(
+			'background',
+			'Поменять цвет фона'
+		);
+		this.#clicksModule = new ClicksModule(
+			'ClicksModule',
+			'Клики',
+			document.body,
+			document.body,
+			5
+		);
+		this.#videoModule = new VideoModule('video', 'Открыть Pedro-енота');
+		this.#contextMenu = new ContextMenu('.menu', [
+			this.#backgroundModule,
+			this.#clicksModule,
+			this.#shapeModule,
+			this.#messageModule,
+			this.#videoModule,
+		]);
+	}
 
-   run() {
-      this.#contextMenu.open();
-      this.#contextMenu.add();
-   }
+	run() {
+		this.#contextMenu.open();
+		this.#contextMenu.add();
+	}
 }
 
 const app = new App();
